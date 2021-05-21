fbdd00c94445a9d2cc1f8df6b9984a2a0be5ff7a
codetann <thetannerthomas@gmail.com>
2021-05-21 12:20:29 -0600

    initial commit

--- profile.js
+++ profile.js
@@ -1,1 +1,17 @@
-const profileBtns = document.querySelector;
+const profileBtns = document.querySelectorAll(".profile-button");
+
+profileBtns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    let favorite = "";
+    switch (e.target.id) {
+      case "color":
+        favorite = "Blue";
+      case "place":
+        favorite = "Hawaii";
+      case "ritual":
+        favorite = "Sleeping";
+    }
+
+    alert(`My favorite ${e.target.id} is ${favorite}`);
+  });
+});
